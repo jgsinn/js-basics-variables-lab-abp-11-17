2f9acedf0ce3f7859bde79983a486a481733791a
Justin Sinn <justin.sinn.co@gmail.com>
2017-11-15 03:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
 var companyName = 'Scuber'
 var companyCeo = 'Susan Smith'
+var mostProfitableNeighborhood = 'Chelsea'
 
 if (companyName != 'Scuber') {
   new Error([message])
